d605029d3898db2e4063a014f5af3f65f6f49b1e
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-07-03 22:46:07 -0300

    useTodos agregado

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -1,23 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export const useForm = (initialForm = {}) => {
-    const [formState, setFormState] = useState(initialForm);
+  const [formState, setFormState] = useState(initialForm);
 
-    const onInputChange = ({ target }) => {
-        const { name, value } = target;
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-    const onResetForm = () => {
-        setFormState(initialForm);
-    };
-    return {
-        ...formState,
-        formState,
-        onInputChange,
-        onResetForm,
-    };
+  const onResetForm = () => {
+    setFormState(initialForm);
+  };
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm,
+  };
 };
--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,48 +1,48 @@
-import { useEffect, useReducer } from 'react';
-import { todoReducer } from '../08-useReducer/todoReducer';
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "./todoReducer";
 
 const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || [];
+  return JSON.parse(localStorage.getItem("todos")) || [];
 };
 
 export const useTodos = () => {
-    const [todos, dispatch] = useReducer(todoReducer, [], init);
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
 
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos]);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-    const handleNewTodo = todo => {
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo,
-        };
-        dispatch(action);
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
     };
+    dispatch(action);
+  };
 
-    const handleDeleteTodo = id => {
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id,
-        });
-    };
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
 
-    const handleToggleTodo = id => {
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id,
-        });
-    };
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
 
-    const todosCount = todos.length;
-    const pendingTodosCount = todos.filter(todo => !todo.done).length;
+  const todosCount = todos.length;
+  const pendingTodosCount = todos.filter((todo) => !todo.done).length;
 
-    return {
-        todos,
-        handleNewTodo,
-        handleDeleteTodo,
-        handleToggleTodo,
-        todosCount,
-        pendingTodosCount,
-    };
+  return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+    todosCount,
+    pendingTodosCount,
+  };
 };
